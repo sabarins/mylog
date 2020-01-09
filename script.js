7c7e0aba4e986a11f09e1e6014ee67b8a7471992
Sabari <ssabari1492000@gmail.com>
2020-01-09 19:30:32 +0530

    fifth commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function check(form)
 {
 if(form.username.value=="kallakurichi" && form.password.value=="thirukoilur")
 {
-window.open('pag.html');
+window.open('https://sabarins.github.io/mylog/pag.html');
 }
 else
 {
