9515904beffdaec05fdf7977c938754ebda445a8
Sabari <ssabari1492000@gmail.com>
2020-01-10 22:46:27 +0530

    ninth commit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function check(form)
 {
 if(form.username.value=="kallakurichi" && form.password.value=="thirukoilur")
 {
-window.open('https://sabarins.github.io/mylog/pag.html', '_blank');
+location.href="https://sabarins.github.io/mylog/pag.html";
 }
 else
 {
-alert("login wrong");
+document.write("login");
 }
 }
